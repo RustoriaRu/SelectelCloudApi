e7de9adaaf112e5666c2fd4dad05fa77822e1de1
vir-mir <virmir49@gmail.com>
2015-07-01 18:16:35 +0300

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='selectel_cloud_api',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     install_requires='selectel_cloud_api',
     url='https://github.com/RustoriaRu/SelectelCloudApi',
