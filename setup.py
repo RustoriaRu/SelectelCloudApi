5d6f52d2b89eda2aa070faafad2fd89eeaf599ec
vir-mir <virmir49@gmail.com>
2015-07-01 16:28:03 +0300

    add setup py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='selectel_cloud_api',
+    version='1.0',
+    packages=find_packages(),
+    install_requires='selectel_cloud_api',
+    url='https://github.com/RustoriaRu/SelectelCloudApi',
+    license='MIT',
+    author='vir-mir',
+    keywords='selectel.ru selectel api, cloud',
+    author_email='virmir49@gmail.com',
+    description='api select cloud api',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
